920132665c1887ea4289cd5a9f88d49f983b21b2
Asik1985 <assylbekezhanuly@yahoo.com>
2023-02-15 19:55:40 +0600

    Добавили логику в program

--- FirstGame/Program.cs
+++ FirstGame/Program.cs
@@ -2,9 +2,38 @@
 
 class Program
 {
+    private static Game _game = new Game();
+
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        while (_game.GetWinner() == Winner.None)
+        {
+            PrintGame();
+            char name = char.Parse(Console.ReadLine());
+            _game.Turn(name);
+            Console.WriteLine(_game.counter);
+        }
+        PrintGame();
+        Winner winner = _game.GetWinner();
+        if (winner == Winner.None)
+        {
+            Console.WriteLine("DRAW");
+        }
+        else if (winner == Winner.Win)
+        {
+            Console.WriteLine("YOU WON");
+        }
+        else
+        {
+            Console.WriteLine("YOU LOSE");
+        }
+        Console.ReadKey(true);
+    }
+
+    static void PrintGame()
+    {
+        Console.WriteLine("GUESS THE WORD");
+        Console.WriteLine(_game.result);
     }
 }
 
